887046335f8c26d3287dd17d0624775d96f853bc
정일 <imbolic0301@gmail.com>
2024-02-25 20:08:11 +0900

    feat : 간단한 쿼리는 MyBatis 의 어노테이션으로 처리한다

--- src/main/java/com/example/persistence/mapper/UserMapper.java
+++ src/main/java/com/example/persistence/mapper/UserMapper.java
@@ -1,12 +1,38 @@
 package com.example.persistence.mapper;
 
 import com.example.persistence.entity.UserEntity;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     // 신규 회원 추가
+    @Insert("""
+            INSERT INTO user_info (
+                name
+                , email
+                , phone
+                , pwd
+                , created_datetime
+            ) VALUES ( 
+                #{name}
+                , #{email} 
+                , #{phone} 
+                , #{password} 
+                , now()
+            )
+            """)
     void createNewUser(UserEntity entity);
+
     // 이메일로 회원 찾기
+    @Select("""
+            SELECT 
+                *
+            FROM 
+                user_info
+            WHERE 
+                email = #{email} AND IS_DEL = 0
+            """)
     UserEntity findByEmail(String email);
 }
