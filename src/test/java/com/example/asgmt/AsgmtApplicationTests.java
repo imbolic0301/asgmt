6a8177e9a8f302f415c5f82ce218f8ff142d6a59
정일 <imbolic0301@gmail.com>
2024-02-25 17:16:24 +0900

    Initial Commit

---
+++ src/test/java/com/example/asgmt/AsgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
